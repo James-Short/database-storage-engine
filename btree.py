e70ccf8a5c5acd976e015eb53b3352c2be907717
James <James.Short.Software@gmail.com>
2026-09-24 16:53:07 -0500

    Built functions for deletion in B-Tree.

--- btree.py
+++ btree.py
@@ -62,3 +62,124 @@ class BTree:
                     i += 1
             self.insert_non_full(x.children[i], k)
     
+    def delete(self, x, k):
+        t = self.t
+        i = 0
+        
+        while i < len(x.keys) and k > x.keys[i]:
+            i += 1
+            
+        if x.leaf:
+            if i < len(x.keys) and x.keys[i] == k:
+                x.keys.pop(i)
+            return
+        
+        if i < len(x.keys) and x.keys[i] == k:
+            return self.delete_internal_node(x, k, i)
+
+        elif len(x.children[i].keys) >= t:
+            self.delete(x.children[i], k)
+        
+        else:
+            if i != 0 and i + 2 < len(x.children):
+                if len(x.children[i-1].keys) >= t:
+                    self.delete_siblings(x, i, i - 1)
+                elif len(x.children[i+1].keys) >= t:
+                    self.delete_sibling(x, i, i + 1)
+                else:
+                    self.delete_merge(x, i, i+1)
+            
+            elif i == 0:
+                if len(x.children[i+1].keys) >= t:
+                    self.delete_sibling(x, i, i+1)
+                else:
+                    self.delete_merge(x, i, i+1)
+            elif i + 1 == len(x.children):
+                if len(x.children[i-1].keys) >= t:
+                    self.delete_sibling(x, i, i-1)
+                else:
+                    self.delete_merge(x, i, i-1)
+            self.delete(x.children[i], k)
+    
+    def delete_internal_node(self, x, k, i):
+        t = self.t
+        if x.leaf:
+            if x.keys[i] == k:
+                x.keys.pop(i)
+            return
+
+        if len(x.children[i].keys) >= t:
+            x.keys[i] = self.delete_predecessor(x.children[i])
+            return
+        
+        elif len(x.children[i+1].keys) >= t:
+            x.keys[i] = self.delete_successor(x.children[i])
+            return
+        else:
+            self.delete_merge(x, i, i+1)
+            self.delete_internal_node(x.children[i], k, self.t - 1)
+    
+    def delete_predecessor(self, x):
+        if x.leaf:
+            return x.keys.pop()
+        n = len(x.keys) - 1
+        if len(x.children[n].keys) >= self.t:
+            self.delete_sibling(x, n+1, n)
+        else:
+            self.delete_merge(x, n, n+1)
+        self.delete_predecessor(x.children[n])
+    
+    def delete_successor(self, x):
+        if x.leaf:
+            return x.keys.pop(0)
+        if len(x.children[1].keys) >= self.t:
+            self.delete_sibling(x, 0, 1)
+        self.delete_successor(x.children[0])
+    
+    def delete_merge(self, x, i, j):
+        cnode = x.children[i]
+        
+        if j > i:
+            rsnode = x.children[j]
+            cnode.keys.append(x.keys[i])
+            for k in range(len(rsnode.keys)):
+                cnode.keys.append(rsnode.keys[k])
+                if len(rsnode.children) > 0:
+                    cnode.children.append(rsnode.children[k])
+            if len(rsnode.children) > 0:
+                cnode.children.append(rsnode.children.pop())
+            new = cnode
+            x.keys.pop(i)
+            x.children.pop(j)
+        else:
+            lsnode = x.children[j]
+            lsnode.keys.append(x.keys[j])
+            for i in range(len(cnode.keys)):
+                lsnode.keys.append(cnode.keys[i])
+                if len(lsnode.children) > 0:
+                    lsnode.children.append(cnode.children[i])
+            if len(lsnode.children) > 0:
+                lsnode.children.append(cnode.children.pop())
+            new = lsnode
+            x.keys.pop(j)
+            x.children.pop(i)
+        
+        if x == self.root and len(x.keys) == 0:
+            self.root = new
+    
+    def delete_sibling(self, x, i, j):
+        cnode = x.children[i]
+        if i < j:
+            rsnode = x.children[j]
+            cnode.keys.append(x.keys[i])
+            x.keys[i] = rsnode.keys[0]
+            if len(rsnode.children) > 0:
+                cnode.children.append(rsnode.children[0])
+                rsnode.children.pop(0)
+            rsnode.keys.pop(0)
+        else:
+            lsnode = x.children[j]
+            cnode.keys.insert(0, x.keys[i - 1])
+            x.keys[i - 1] = lsnode.keys.pop()
+            if len(lsnode.children) > 0:
+                cnode.children.insert(0, lsnode.children.pop())
